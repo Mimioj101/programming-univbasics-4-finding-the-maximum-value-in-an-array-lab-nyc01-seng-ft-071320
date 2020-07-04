e05d6c558c8b3bb7ea3f7b4781c3ef6e1cb0bb1f
Mimi Ojserkis <mimioj@gmail.com>
2020-07-04 15:14:48 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,8 +1,8 @@
 def find_max_value(array)
   counter = 0 
     while counter < array.length do
-     array[counter] < array[counter + 1]
-     puts array[counter]
-         counter +=1 
+      array[counter] < array[counter + 1]
+      puts array[counter]
+      counter +=1 
     end
 end
